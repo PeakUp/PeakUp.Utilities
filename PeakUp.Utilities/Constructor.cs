7c6d612ebeff8a024b9d1c88bfa6433f7646ad5c
PeakUp <fth.dgn.369@gmail.com>
2017-10-19 20:14:31 +0300

    Added constructor class.

---
+++ PeakUp.Utilities/Constructor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakUp.Utilities
+{
+    public abstract class Constructor<T>
+    {
+        public Constructor(T parameter) { }
+    }
+}

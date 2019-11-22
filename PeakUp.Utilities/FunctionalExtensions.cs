56a4d7b9afb34d126e964f8a347d32bedd5d501d
Fatih Memis <fatih.memis@peakup.org>
2019-11-22 10:00:49 +0300

    IsNullOrEmpty extensions added for string, IEnumerable, IList, IEnumerable<T> and IList<T>

--- PeakUp.Utilities/FunctionalExtensions.cs
+++ PeakUp.Utilities/FunctionalExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PeakUp.Utilities
 {
@@ -14,5 +17,10 @@ namespace PeakUp.Utilities
 
         public static bool IsNull<T>(this T self) where T : class => self == null;
         public static bool IsNull<T>(this T? self) where T : struct => !self.HasValue;
+        public static bool IsNullOrEmpty(this string self) => string.IsNullOrEmpty(self);
+        public static bool IsNullOrEmpty<T>(this IList<T> self) => self == null || self.Count == 0;
+        public static bool IsNullOrEmpty(this IList self) => self == null || self.Count == 0;
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> self) => self == null || !self.Any();
+        public static bool IsNullOrEmpty(this IEnumerable self) => self == null || !self.GetEnumerator().MoveNext();
     }
 }
